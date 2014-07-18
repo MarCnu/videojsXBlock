5a2a56092da64c38115636794498d8affb33e13a
MarCnu <marceau.cnudde@gmail.com>
2014-07-18 15:03:47 +0200

    Update videojs_view.js

--- videojs/static/js/videojs_view.js
+++ videojs/static/js/videojs_view.js
@@ -6,10 +6,6 @@ function videojsXBlockInitView(runtime, element) {
      * So here I make sure element is the jQuery object */
     if(element.innerHTML) element = $(element);
     
-    // To include data-setup inside the HTML, with the Python string builder :
-    // data-setup="{{&quot;playbackRates&quot;:[0.75,1,1.25,1.5,1.75,2]}}"
-    console.log('Init videojsXBlock view');
-    
     var video = element.find('video:first');
     videojs(video.get(0), {playbackRates:[0.75,1,1.25,1.5,1.75,2]}, function() {});
 
